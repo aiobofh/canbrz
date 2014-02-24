06278631b4f87a70cd1524ce6f380850fd898668
Joakim Ekblad <aio.github@aio.nu>
2014-02-24 22:42:23 +0100

    Removed the alpha line, since it did'nt work on the n900 and made the demo sequence end with a slowly increasing temperature

--- src/canbrz.py
+++ src/canbrz.py
@@ -31,6 +31,7 @@ water_blink = False
 ticks=0
 angle=1.6
 angle=3.14 * 2
+old_water_value=0
 water_value=40
 intro=True
 intro_reverse=False
@@ -39,51 +40,6 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    screen.fill(BLACK)
-
-    if water_value > 140:
-        water_value = 140
-    if water_value < 40:
-        water_value = 40
-
-    water_blink = (water_value > 120)
-
-    if water_blink == True and ticks < 5:
-        screen.blit(water_warning, [70, 0])
-    else:
-        screen.blit(water, [70, 0])
-
-    angle = (water_value - 40) / 21.37
-
-    percent = angle + 1.6
-
-    # Rotate all points making up the pointer
-    x0 = math.cos(percent) * (INNERRADIUS + 2)
-    y0 = math.sin(percent) * (INNERRADIUS + 2)
-    x1 = math.cos(percent + 0.03) * INNERRADIUS
-    y1 = math.sin(percent + 0.03) * INNERRADIUS
-    x2 = math.cos(percent) * OUTERRADIUS
-    y2 = math.sin(percent) * OUTERRADIUS
-    x3 = math.cos(percent - 0.03) * INNERRADIUS
-    y3 = math.sin(percent - 0.03) * INNERRADIUS
-    x4 = math.cos(percent) * (OUTERRADIUS - 2)
-    y4 = math.sin(percent) * (OUTERRADIUS - 2)
-
-    pygame.draw.polygon(screen, RED, [[320 + x1, 250 + y1],
-                                      [320 + x2, 250 + y2],
-                                      [320 + x3, 250 + y3]], 5)
-    pygame.draw.aaline(screen,
-                       LIGHTRED,
-                       [320 + x0, 250 + y0], [320 + x4, 250 + y4], True)
-
-    # Swap buffer
-    pygame.display.flip()
-
-    ticks += 1
-    if ticks == 10:
-        ticks = 0
-    if ticks % 2 == 0:
-        pass
 
     # Silly BRZ style intro sequence.
     if intro == True:
@@ -96,8 +52,54 @@ while not done:
             if water_value == 40:
                 intro_reverse = False
                 intro = False
+    else:
+       water_value += 0.04 # Just for demo
+
+    if water_value > 140:
+        water_value = 140
+    if water_value < 40:
+        water_value = 40
+
+    water_blink = (water_value > 120)
+
+#    if water_blink == False and (water_value == old_water_value):
+#        clock.tick(5000)
+#        water_value += 1
+#        continue
+
+    angle = (water_value - 40) / 21.37
+
+    percent = angle + 1.6
+
+    # Rotate all points making up the pointer
+    x1 = math.cos(percent + 0.03) * INNERRADIUS
+    y1 = math.sin(percent + 0.03) * INNERRADIUS
+    x2 = math.cos(percent) * OUTERRADIUS
+    y2 = math.sin(percent) * OUTERRADIUS
+    x3 = math.cos(percent - 0.03) * INNERRADIUS
+    y3 = math.sin(percent - 0.03) * INNERRADIUS
+
+    screen.fill(BLACK)
+
+    if water_blink == True and ticks < 5:
+        screen.blit(water_warning, [70, 0])
+    else:
+        screen.blit(water, [70, 0])
+
+    pygame.draw.polygon(screen, RED, [[320 + x1, 250 + y1],
+                                      [320 + x2, 250 + y2],
+                                      [320 + x3, 250 + y3]], 5)
+    # Swap buffer
+    pygame.display.flip()
+
+    ticks += 1
+    if ticks == 10:
+        ticks = 0
+    if ticks % 2 == 0:
+        pass
 
     # Wait for a relevant time (PAL)
     clock.tick(50)
+    old_water_value = water_value
 
 pygame.quit()
